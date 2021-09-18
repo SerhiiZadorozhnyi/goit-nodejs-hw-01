c8827b19c24ce92070328ad7b4e239e9ec4173fe
Serhii <71183966+SerhiiZadorozhnyi@users.noreply.github.com>
2021-09-18 18:35:46 +0300

    Final result

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # goit-nodejs-hw-01
+
+List
+![action-list](screenshots/01_action.list.png "Описание будет тут")
+Get
+![action-get-byid](screenshots/02_get.png "Описание будет тут")
+Add
+![action-add](screenshots/03_add.png "Описание будет тут")
+Delete
+![action-delete](screenshots/04_delete.png "Описание будет тут")
--- contacts.js
+++ contacts.js
@@ -1,23 +1,54 @@
-// contacts.js
+const fs = require('fs').promises;
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
-/*
- * Раскомментируй и запиши значение
- * const contactsPath = ;
- */
 
-// TODO: задокументировать каждую функцию
+const contactsPath = path.resolve("db/contacts.json");
+
 function listContacts() {
-    // ...твой код
-  }
+  fs.readFile(contactsPath)
+    .then((data) => console.table(JSON.parse(data.toString())))
+    .catch((err) => console.log(err.message))
+}
   
-  function getContactById(contactId) {
-    // ...твой код
-  }
-  
-  function removeContact(contactId) {
-    // ...твой код
-  }
-  
-  function addContact(name, email, phone) {
-    // ...твой код
-  }
+function getContactById(contactId) {
+  fs.readFile(contactsPath)
+    .then((data) => 
+      JSON.parse(data.toString()).find(
+        (contact) => 
+          contact.id.toString() === contactId && console.table(contact)
+      )
+    )
+    .catch((err) => console.log(err.message))
+}
+
+function removeContact(contactId) {
+  fs.readFile(contactsPath)
+    .then((data) => {
+      const contacts = JSON.parse(data).filter(
+        (contact) => contact.id.toString() !== contactId
+      )
+      fs.writeFile(contactsPath, JSON.stringify(contacts))
+        .then(() => 
+          console.log(`contact ${contactId} was successfully deleted`)
+        )
+        .catch((err) => console.log(err.message))
+    })
+    .catch((err) => console.log(err.message))
+}
+
+function addContact(name, email, phone) {
+  fs.readFile(contactsPath)
+    .then((data) => {
+      const contacts = JSON.parse(data)
+      const id = uuidv4()
+      const newContact = { id, name, email, phone }
+      contacts.push(newContact)
+      fs.writeFile(contactsPath, JSON.stringify(contacts))
+        .then(() => console.log(`contact was successfully added`))
+        .catch((err) => console.log(err.message))
+    })
+    .catch((err) => console.log(err.message))
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact }
